11783b11936cd7b246e7337c34cc33a0717022eb
gdcp <gdcp@m44.local>
2018-03-12 11:38:40 +0800

    最后修改的代码

--- UIButtonDemo/ViewController.swift
+++ UIButtonDemo/ViewController.swift
@@ -12,7 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
+        button.backgroundColor = UIColor.red
+        button.setTitle("点我一下", for: .normal)
+        button.addTarget(self, action: #selector(ViewController.changeColor), for: .touchUpInside)
+        self.view.addSubview(button)
+
+    }
+    func changeColor(){
+        self.view.backgroundColor = UIColor(red: CGFloat(arc4random()%256)/255, green: CGFloat(arc4random()%256)/255, blue: CGFloat(arc4random()%256)/255, alpha: CGFloat(arc4random()%256)/255)
+        
     }
 
     override func didReceiveMemoryWarning() {
